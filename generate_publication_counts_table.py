ab7ae9f15e2fe6b37a87dfad0597124d3d02bc53
Florian Pfaff <pfaff@kit.edu>
2023-04-16 17:38:01 +0200

    Added generate_publication_counts_table.py

---
+++ generate_publication_counts_table.py
@@ -0,0 +1,188 @@
+import warnings
+import bibtexparser
+from bibtexparser.bparser import BibTexParser
+from bibtexparser.customization import author, convert_to_unicode
+import argparse
+
+ENABLEEXTRAS = False
+
+# create map between ids and table headers
+NAMES = {'article': 'Journals', 
+     'inproceedings': 'Conferences',
+     'inbook': 'In Books',
+     'incollection': 'In Books',
+     'book': 'Editorship',
+     'proceedings': 'Editorship',
+     'phdthesis': 'Books and Theses',
+       }
+
+PREPRINTS_STR = 'Preprints'          # name of 'preprints' header
+PREPRINTS_FILE_STR = 'Preprints'          # string that, if file name contains it, marks file as preprints
+AUTHOR_STR = 'Hanebeck, Uwe D.'          # name that must be contained in authors field
+MIN_YEAR = '1989'                # minimum year to display
+# create list of table headers
+class Lang: pass
+
+TABLE_HEADERS = [ 'Conferences', 'Journals', 'In Books', 'Editorship', 'Books and Theses', 'Preprints', 'Other']
+if ENABLEEXTRAS:
+    special_headers = ['Fusion', 'MFI']
+else:
+    special_headers = []
+FullHeaders = TABLE_HEADERS[0:1] + special_headers + TABLE_HEADERS[1:]
+
+special_header_strings = {
+    'Fusion': 'International Conference on Information Fusion',
+    'MFI': 'International Conference on Multisensor Fusion'
+    }
+
+en = Lang()
+en.TableHeaders = [ '<div align="left"><div class="balken-inproceedings" style="align:left; width:10px; height:10px; display: inline-block;"></div> Conferences </div>', 
+    '<div align="left"><div class="balken-article" style="width:10px; height:10px; display: inline-block;"></div> Journals </div>', 
+    '<div align="left"><div class="balken-inbook" style="width:10px; height:10px; display: inline-block;"></div> In Books </div>', 
+    '<div align="left"><div class="balken-book" style="width:10px; height:10px; display: inline-block;"></div> Editorship </div>', 
+    '<div align="left"><div class="balken-phdthesis" style="width:10px; height:10px; display: inline-block;"></div> Books and Theses </div>', 
+    '<div align="left"><div class="balken-preprint" style="width:10px; height:10px; display: inline-block;"></div> Preprints </div>', 
+    '<div align="left"><div class="balken-other" style="width:10px; height:10px; display: inline-block;"></div> Other </div>']
+if ENABLEEXTRAS:
+    en.SpecialHeaders = ['<div class="balken-fusion" style="width:10px; height:10px; display: inline-block;"></div> <div style="text-indent:20px;width:10px; height:10px; display: inline-block;"><span title="Fusion: International Conference on Information Fusion">Fusion</span></div>',
+        '<div class="balken-mfi" style="width:10px; height:10px; display: inline-block;"></div> <div style="text-indent:20px;width:10px; height:10px; display: inline-block;"><span title="MFI: IEEE International Conference on Multisensor Fusion and Integration for Intelligent Systems">MFI</span></div>']
+else:
+    en.SpecialHeaders = []
+en.FullHeaders = en.TableHeaders[0:1] + en.SpecialHeaders + en.TableHeaders[1:]
+
+    
+en.TypeYear = '<div style="width:125px; height:10px; display: inline-block;">Type / Year</div>'
+en.Total = '<div align="left">Total</div>'
+
+de = Lang()
+de.TableHeaders = [ '<div align="left"><div class="balken-inproceedings" style="width:10px; height:10px; display: inline-block;"></div> Konferenzen </div>', 
+    '<div align="left"><div class="balken-article" style="width:10px; height:10px; display: inline-block;"></div> Zeitschriften </div>',
+    '<div align="left"><div class="balken-inbook" style="width:10px; height:10px; display: inline-block;"></div> In Büchern </div>', 
+    '<div align="left"><div class="balken-book" style="width:10px; height:10px; display: inline-block;"></div> Editor von Büchern oder Zeitschriften </div>', 
+    '<div align="left"><div class="balken-phdthesis" style="width:10px; height:10px; display: inline-block;"></div> Bücher und Thesen </div>', 
+    '<div align="left"><div class="balken-preprint" style="width:10px; height:10px; display: inline-block;"></div> Preprints </div>', 
+    '<div align="left"><div class="balken-other" style="width:10px; height:10px; display: inline-block;"></div> Andere </div>']
+
+de.SpecialHeaders = en.SpecialHeaders
+de.FullHeaders = de.TableHeaders[0:1] + de.SpecialHeaders + de.TableHeaders[1:]
+
+de.TypeYear = "Typ / Jahr"
+de.Total = "Gesamt"
+
+Langs = [ en ]
+def parse_bib_files(filelist):
+  """
+    Parse the given list of BibTeX files and return a catalog with publication counts.
+
+    Args:
+        filelist (list): List of BibTeX file names to process.
+
+    Returns:
+        dict: A dictionary containing publication counts by year and type.
+    """
+  catalog = dict()
+
+  # parse bibtex files
+  for file in filelist:
+    with open(file) as bibtex_file:
+      parser = BibTexParser(common_strings=True)
+      parser.customization = convert_to_unicode
+      parser.customization = author
+      
+      bibdata = bibtexparser.load(bibtex_file, parser=parser)
+        
+    isPreprints = (PREPRINTS_FILE_STR in file)
+
+    for entry in bibdata.entries:
+    
+      if (isPreprints):
+        etype = PREPRINTS_STR
+      else:
+        bibType = entry['ENTRYTYPE']
+        try:
+          etype = NAMES[bibType]
+        except KeyError:
+          etype = "Other"
+      
+      try:
+        year = entry['year']
+      except KeyError:
+        print (f"Entry '{entry.key}' has no year!")
+        continue
+
+      if year < MIN_YEAR:
+        continue
+
+      if "author" in entry.keys():
+          authors = entry["author"]
+          if "editor" in entry.keys():
+            authors += entry["editor"]
+      elif "editor" in entry.keys():  # Use editor instead of author if no author is available
+          authors = entry["editor"]
+      else:  # Entry has neither author nor editor, this must not happen
+          raise
+
+      if not any("Hanebeck" in name for name in authors) and not "Hanebeck" in authors:
+        warnings.warn(f'Publication {entry["ID"]} is not by Hanebeck but by {authors}')
+        continue
+      
+      if year not in catalog:
+        catalog[year] = dict()
+      
+      if etype not in catalog[year]:
+        catalog[year][etype] = set()
+
+      catalog[year][etype].add(entry['ID'])
+
+      if entry['ENTRYTYPE'] == "inproceedings":
+        try:
+            title = entry['booktitle']
+        except KeyError:
+            continue
+      for conf in special_headers:
+          confString = special_header_strings[conf]
+          if confString in title:
+              if not conf in catalog[year]:
+                  catalog[year][conf] = set()
+              catalog[year][conf].add(entry['ID'])
+    return catalog
+def create_counts_table(output_type, filelist):
+  """
+  Generate the table from the publication counts
+
+  Args:
+      output_type (str): Output type, either "full" or "simple".
+      filelist (list): List of BibTeX file names to process.
+  """
+  catalog = parse_bib_files(filelist)
+  numFullHeaders = len(FullHeaders)
+
+  years = sorted(catalog.keys(), reverse=True)
+  numYears = len(years)
+
+  data = [ [ (len(catalog[year][header]) if (header in catalog[year]) else 0) for year in years ] for header in TABLE_HEADERS ] 
+  headerTotals = [ sum(row) for row in data ] 
+  yearTotals = [ sum(row[y] for row in data) for y in range(0, numYears) ] 
+
+  fullData = [ [ (len(catalog[year][header]) if (header in catalog[year]) else 0) for year in years ] for header in FullHeaders ] 
+  fullHeaderTotals = [ sum(row) for row in fullData ] 
+
+  if output_type == "full":
+    print(f"{en.TypeYear},<b>Total</b>," + ",".join( (f'<a href="#{year}">{year}</a>') for year in years))
+    for h in range(0, numFullHeaders):
+      print(f"{','.join(lang.FullHeaders[h] for lang in Langs)},<b>{fullHeaderTotals[h]}</b>," + ",".join(str(yt) for yt in fullData[h]))
+    print(f"{','.join(lang.Total for lang in Langs)},<b>{sum(yearTotals)}</b>," + ",".join(str(yt) for yt in yearTotals))
+  elif output_type == "simple":
+    print(f"{','.join(TABLE_HEADERS)},Total")
+    print(f"{','.join(str(ht) for ht in headerTotals)},{sum(yearTotals)}")
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser(description="Generate publication counts from BibTeX files.")
+  parser.add_argument("--output_type", choices=["full", "simple"], default="full", help="Choose output type: full or simple.")
+  parser.add_argument("filelist", nargs="+", help="List of BibTeX files to process.")
+  args = parser.parse_args()
+
+  output_type = args.output_type
+  filelist = args.filelist
+
+  create_counts_table(args.output_type, args.filelist)
